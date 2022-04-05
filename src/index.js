fa0b1dd50af64d94d1fc8ffa06a759356f593e51
Kenan Salic <kenan.salic@bloomreach.com>
2022-04-05 17:11:10 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import UiExtension from "@bloomreach/ui-extension";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Dialog from "./Dialog";
+
+const rootElement = document.getElementById("root");
 
 document.addEventListener('DOMContentLoaded', async () => {
     try {
@@ -12,30 +14,29 @@ document.addEventListener('DOMContentLoaded', async () => {
 
         console.log('ui..', ui);
 
-        const routing = (
+        render(
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<App/>}/>
                     <Route path="/dialog" element={<Dialog/>}/>
                 </Routes>
-            </BrowserRouter>
+            </BrowserRouter>,
+            rootElement
         );
 
-        ReactDOM.render(routing, document.getElementById("root"));
     } catch (error) {
         console.log(error);
         console.error('Failed to register extension:', error.message);
         console.error('- error code:', error.code);
-        const routing = (
+        render(
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<App/>}/>
                     <Route path="/dialog" element={<Dialog/>}/>
                 </Routes>
-            </BrowserRouter>
+            </BrowserRouter>,
+            rootElement
         );
-
-        ReactDOM.render(routing, document.getElementById('root'));
     }
 });
 
