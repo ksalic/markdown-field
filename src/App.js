16e736429d8b5717c686f79fc0d878909eb706ca
Kenan Salic <kenan.salic@bloomreach.com>
2022-04-05 16:37:24 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import {TextareaAutosize} from "@mui/material";
+// import Stackedit from 'stackedit-js';
+import {Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const stackedit = new Stackedit();
+
+class App extends Component {
+
+
+    constructor(props) {
+        super(props)
+
+        this.ui = props.ui;
+
+        this.state = {text: '', mode: 'view'};
+    }
+
+    componentDidMount() {
+        this.getInitialState(this.ui).then(state => this.setState(state));
+    }
+
+    async getInitialState(ui) {
+        try {
+            const brDocument = await ui.document.get();
+            const value = await ui.document.field.getValue();
+            return {mode: brDocument.mode, text: value};
+        } catch (error) {
+            console.error('Failed to register extension:', error.message);
+            console.error('- error code:', error.code);
+        }
+        return this.state;
+    }
+
+    async openDialog() {
+        try {
+            this.dialogOptions = {
+                title: 'Markdown Editor',
+                url: './dialog',
+                size: 'large',
+                value: this.state.text
+            };
+
+            const response = await this.ui.dialog.open(this.dialogOptions);
+            await this.ui.document.field.setValue(response);
+            this.setState({text: response})
+        } catch (error) {
+            if (error.code === 'DialogCanceled') {
+                return;
+            }
+            console.error('Error after open dialog: ', error.code, error.message);
+        }
+
+    }
+
+    render() {
+        const {text, mode} = this.state;
+        console.log(mode);
+        return (
+            <>
+                <TextareaAutosize value={text} onChange={event => {
+                    this.setState({text: event.target.value})
+                }} minRows={5} id={''} style={{width: '100%',}}/>
+                <div className="stackedit-button-wrapper">
+                    <span onClick={(e) => {
+                        e.preventDefault();
+                        this.openDialog();
+                    }}>
+                        <img alt={'stackedit button to open editor'} src="/stackedit-logo.svg"/>Edit with
+                        StackEdit
+                    </span>
+                </div>
+            </>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UiExtension from "@bloomreach/ui-extension";
+import {Route, Router} from "react-router-dom";
+import {Switch} from "@mui/material";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+document.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const ui = await UiExtension.register();
+
+        const routing = (
+            <Router>
+                <Switch>
+                    {/*<Route path="/dialog" render={props => <ExtPickerDialog ui={ui}/>}/>*/}
+                    <Route exact path="/" render={props => <App ui={ui}/>}/>
+                </Switch>
+            </Router>
+        );
+
+        ReactDOM.render(routing, document.getElementById("root"));
+
+    } catch (error) {
+        console.log(error);
+        console.error('Failed to register extension:', error.message);
+        console.error('- error code:', error.code);
+        ReactDOM.render(<App/>, document.getElementById('root'));
+    }
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
